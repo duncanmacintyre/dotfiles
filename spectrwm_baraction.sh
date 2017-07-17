dbee9dd069967967378e84aacbc515e2658ceb4a
duncanmacintyre <duncanmacintyre@users.noreply.github.com>
2017-07-17 13:45:28 -0700

    Adds battery indicator to status bar

--- spectrwm_baraction.sh
+++ spectrwm_baraction.sh
@@ -1,5 +1,14 @@
-#
-# ~/.config/spectrwm/baraction.sh
-#
 #!/bin/bash
-echo test
+#
+# ~/bin/baraction.sh
+#
+# script for spectrwm bar
+#
+
+SLEEP_SEC=5  # set bar_delay = 5 in /etc/spectrwm.conf
+
+# loops forever outputting a line every SLEEP_SEC secs
+while :; do
+        acpi | sed 's/.*\([0-9]\{2,3\}%\).*/\1/'
+        sleep $SLEEP_SEC
+done
